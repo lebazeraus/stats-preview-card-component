d63ec2340906211d5e618bfb5126bca8b8b82192
lebazeraus <lebazeraus@users.noreply.github.com>
2021-07-10 23:37:47 +0000

    deploy: 938262948c59714773aba5021888106d53a8518b

--- _nuxt/static/1625885425/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fstats-preview-card-component\u002F_nuxt\u002Fstatic\u002F1625885425",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fstats-preview-card-component\u002F",assetsPath:"\u002Fstats-preview-card-component\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
